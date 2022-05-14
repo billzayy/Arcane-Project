6faeb09e6b27905ec5aba67ceb6121446ce32e9c
Bill Zay <76415515+billzayy@users.noreply.github.com>
2022-05-14 21:03:34 +0700

    Preparation for DB

---
+++ Arcane DB.session.sql
@@ -0,0 +1,55 @@
+-- CREATE TABLE Login(
+--     Id_Login    INT             PRIMARY KEY     AUTO_INCREMENT   NOT NULL,
+--     UserName    VARCHAR(255)    NOT NULL,
+--     Password    VARCHAR(255)    NOT NULL,
+--     FullName    VARCHAR(255)    NULL,
+--     Picture     Varchar(255)    NULL
+-- );
+
+-- CREATE TABLE Contact(
+--     Work_hour   Nvarchar(255)   NOT NULL,
+--     Address     NVARCHAR(255)   NOT NULL,
+--     Email       Nvarchar(255)   NOT NULL,
+--     Phone       NvARCHAR(11)    NOT NULL
+-- );
+
+-- CREATE TABLE Product(
+--     Id_Product      INT             PRIMARY KEY     NOT NULL,
+--     P_Name          NVARCHAR(255)   NOT NULL,
+--     P_Description   NVARCHAR(255)   NOT NULL,
+--     P_Price         INT             NOT NULL,
+--     P_Category      NVARCHAR(255)   NOT NULL,
+--     P_Sales         BOOLEAN         NULL,
+--     P_Top           BOOLEAN         NULL,
+--     P_New           BOOLEAN         NULL,
+--     P_Material      NVARCHAR(255)   NOT NULL,
+--     P_Manufacturer  NVARCHAR(255)   NOT NULL,
+--     P_Colour        NVARCHAR(255)   NOT NULL,
+--     P_Size          INT             NOT NULL,
+--     Content_1       NVARCHAR(255)   NOT NULL,
+--     Content_2       NVARCHAR(255)   NOT NULL
+-- );
+
+-- CREATE TABLE Bills(
+--     Id_Bill         INT     PRIMARY KEY     NOT NULL,
+--     Id_Product      INT     NOT NULL,
+--     Id_Client       INT     NOT NULL,
+--     B_BuyDate       Nvarchar(255)   NOT NUll,
+--     FOREIGN KEY(Id_Product) REFERENCES Product(Id_Product),
+--     FOREIGN KEY(Id_Client) REFERENCES Login(Id_Login)
+-- );
+
+-- CREATE TABLE Bills_Info(
+--     Id_Bill         INT         NOT NULL,
+--     Id_Product      INT         NOT NULL,
+--     P_SumPrice      INT         NOT NULL,
+--     B_Quantity      INT         NOT NULL,
+--     B_Discount      INT         NOT NULL,
+--     B_Delivery      NVARCHAR(55) NOT NULL,
+--     FOREIGN KEY (Id_Bill) REFERENCES Bills(Id_Bill),
+--     FOREIGN KEY (Id_Product) REFERENCES Product(Id_Product)
+-- );
+
+INSERT INTO Login(UserName,Password,FullName,Picture) VALUES
+('admin','admin',NULL,NULL),
+('billzay','20102002','Phan Le Tuan','./src/img/User/Bill .jpeg')
--- README.md
+++ README.md
@@ -19,6 +19,18 @@ To use this project, you need to use Docker command on your terminal to build:
 ```
 docker-compose up -d
 ```
-<span style="color:tomato;font-weight:700;font-size:30px">
-    Have a nice day with coding !!!
-</span>
+--------------------------------
+## Database:
+Tables :
+- Login ( Id_Login, UserName, Password, FullName, Picture )
+- Product ( Id_Product, P_Name, P_Describe, P_Price, P_Category, P_Sales, P_Top, P_New, P_Material, P_Manufacture, P_Colour, P_Size , Content_1, Content_2 )
+- Bills ( Id_Bill ,Id_Product, Id_UserName, B_BuyDate )
+- Bills_Info ( Id_Bill ,Id_Product, P_Price, B_Quantity, B_Discount, B_Delivery )
+- Contact ( Work_hour, Address, E-mail, Phone)
+
+Diagram :
+![alt text](/Diagram_Picture.png)
+
+
+
+## Have a nice day for Coding !!!
