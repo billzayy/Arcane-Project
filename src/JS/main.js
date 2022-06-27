c91e34b936485b8fd18a4e0c1bda86af5ddf0f75
Bill Zay <76415515+billzayy@users.noreply.github.com>
2022-06-27 14:13:41 +0700

    Finish Content of Main Page

--- src/JS/main.js
+++ src/JS/main.js
@@ -10,8 +10,8 @@ bar.addEventListener('click', () => {
 
 window.addEventListener("DOMContentLoaded", () => { 
     mainClothes();
-    // mainDeco();
-    // mainOther();
+    mainDeco();
+    mainOther();
 })
 
 function mainClothes() { 
@@ -23,14 +23,14 @@ function mainClothes() {
                 if (i.P_Page == 'Main' && i.P_Category == 'Clothes') {
                     mainPage += `
                     <section>
-                        <div class="clothes-pic">
+                        <div class="content-pic">
                             <div class="special">${i.P_Sales}</div>
                             <img src="${i.P_Picture}" alt="">
                             <div class="price">$${i.P_Price}</div>
                         </div>
-                        <div class="clothes-text">
-                            <div class="clothes-description">${i.P_Title}</div>
-                            <div class="clothes-name">${i.P_Name}</div>
+                        <div class="content-text">
+                            <div class="content-description">${i.P_Title}</div>
+                            <div class="content-name">${i.P_Name}</div>
                         </div>
                     </section>
                         `
@@ -50,6 +50,77 @@ function mainClothes() {
         })
 }
 
+function mainDeco() {
+    fetch("http://localhost:3000/api")
+        .then(res => res.json())
+        .then(data => {
+            let mainPage = "";
+            data.forEach(i => {
+                if (i.P_Page == 'Main' && i.P_Category == 'Deco') {
+                    mainPage += `
+                    <section>
+                        <div class="content-pic">
+                            <div class="special">${i.P_Sales}</div>
+                            <img src="${i.P_Picture}" alt="">
+                            <div class="deco-price">$${i.P_Price}</div>
+                        </div>
+                        <div class="content-text">
+                            <div class="content-description">${i.P_Title}</div>
+                            <div class="content-name">${i.P_Name}</div>
+                        </div>
+                    </section>
+                        `
+                }
+            })
+            return mainPage;
+        })
+        .then(result => {
+            deco.innerHTML = result
+        })
+        .then(() => {
+            const picTest = document.querySelectorAll("#deco img");
+
+            picTest.forEach(i => {
+                console.log(i.clientWidth, i.clientHeight)
+            })
+    })
+}
+
+function mainOther() {
+    fetch("http://localhost:3000/api")
+        .then(res => res.json())
+        .then(data => {
+            let mainPage = "";
+            data.forEach(i => {
+                if (i.P_Page == 'Main' && i.P_Category == 'Other') {
+                    mainPage += `
+                    <section>
+                        <div class="content-pic">
+                            <div class="special">${i.P_Sales}</div>
+                            <img src="${i.P_Picture}" alt="">
+                            <div class="price">$${i.P_Price}</div>
+                        </div>
+                        <div class="content-text">
+                            <div class="content-description">${i.P_Title}</div>
+                            <div class="content-name">${i.P_Name}</div>
+                        </div>
+                    </section>
+                        `
+                }
+            })
+            return mainPage;
+        })
+        .then(result => {
+            other.innerHTML = result
+        })
+        .then(() => {
+            const price = document.querySelectorAll('#other .price');
+            const sale = document.querySelectorAll('#other .special');
+            for (let i = 0; i < price.length; i++) {
+                changeColorSale(sale[i], price[i]);
+            }
+        })
+}
 
 function changeColorSale(sale, price) {
     if (sale.textContent == "Sale") {
